0038cdde53b9822b8b2239231c272c33523b45be
OZHAN YILDIRIM <48368932+ozhanyildirim@users.noreply.github.com>
2021-05-15 14:55:59 +0300

    login and admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyCv.Models.Entity;
+using MyCv.Repositories;
+
+namespace MyCv.Controllers
+{
+    
+    public class AdminController : Controller
+    {
+        GenericRepository<Admin> repos = new GenericRepository<Admin>();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var list = repos.List();
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(Admin p)
+        {
+            repos.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AdminDelete(int id)
+        {
+            Admin tx = repos.Find(x => x.ID == id);
+            repos.TDelete(tx);
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult AdminEdit(int id)
+        {
+            Admin tx = repos.Find(x => x.ID == id);
+            return View(tx);
+        }
+
+        [HttpPost]
+        public ActionResult AdminEdit(Admin p)
+        {
+            Admin tx = repos.Find(x => x.ID == p.ID);
+            tx.UserName = p.UserName;
+            tx.Password = p.Password;
+            
+            repos.TUpdate(tx);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using MyCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MyCv.Controllers
+{   
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin p)
+        {
+            DbCVEntities db = new DbCVEntities();
+            var root = db.Admin.FirstOrDefault(x => x.UserName == p.UserName && x.Password == p.Password);
+            if(root != null)
+            {
+                FormsAuthentication.SetAuthCookie(root.UserName, false);
+                Session["UserName"] = root.UserName.ToString();
+                return RedirectToAction("Index", "Experience");
+
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
